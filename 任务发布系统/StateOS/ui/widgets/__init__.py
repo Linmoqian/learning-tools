573679a387756d03c3dbc0a9d2c8430f18d31e8c
Linmoqian <3047076929@qq.com>
2026-06-02 16:39:45 +0800

    chore: 清理旧版 Python 工程（任务发布系统、笔记处理系统）

--- 任务发布系统/StateOS/ui/widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-StateOS 自定义UI组件
-"""
-
-__version__ = "1.0.0"
-__all__ = ["StatusIndicator", "TacticalButton"]
-
-from .status_indicator import StatusIndicator
-from .tactical_button import TacticalButton
--- 任务发布系统/StateOS/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-StateOS 工具函数模块
-"""
-
-__version__ = "1.0.0"
-__all__ = ["helpers", "validators"]
-
-from . import helpers
-from . import validators
